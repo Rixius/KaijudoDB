c71e4a099cd94120a62a1c1b054176fd9069c440
Stephen "Rixius" Middleton <stephen+git@rixi.us>
2014-04-19 07:21:37 -0500

    Mostly searching successfully.

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,9 +1,37 @@
 class CardController < ApplicationController
-  def show
-      @card = Card.find_by_slug params[:slug]
-  end
-  def search
-      @query = params[:q]
-
-  end
+    def show
+        @card = Card.find_by_slug params[:slug]
+    end
+    def search
+        @civs = {
+            'L' => 'Light',
+            'W' => 'Water',
+            'D' => 'Darkness',
+            'F' => 'Fire',
+            'N' => 'Nature'
+        }
+        @queryparts = params[:q].gsub(/"(\w+) (\w+)"/, '"\1☃\2"').split
+        @query = Card.order('name')
+        @queryparts.each do |part|
+            part.gsub!(/"(\w+)☃(\w+)"/, '\1 \2')
+            if part[':']
+                part = part.split ':'
+                case part.first
+                when "civ"
+                    @query = @query.joins(:civs)
+                    .where('civs.id IN (?)', part.last.split('').map do |civshort|
+                        Civ.find_by_name @civs[civshort]
+                    end)
+                end
+            elsif part.match(/[<>=]/)
+                part = /(.+)([<>=]{1,2})(.+)/.match(part)
+                case part[1]
+                when 'cost', 'power'
+                    @query = @query.where("#{part[1]} #{part[2]} ?", Integer(part[3]))
+                end
+            else
+                @query = @query.where('cards.name LIKE ?', "%"+part+"%")
+            end
+        end
+    end
 end

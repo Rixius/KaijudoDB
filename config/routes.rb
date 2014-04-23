d7484567fae93eb040ffc5bf08d9339168f43881
Stephen "Rixius" Middleton <stephen+git@rixi.us>
2014-04-23 13:04:32 -0500

    Added and configured devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+      get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  root to: 'card#index'
   get 'card' => 'card#index'
   get 'card/show/:slug' => 'card#show'
   get 'card/search'

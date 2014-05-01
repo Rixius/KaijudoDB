00eed5a47e7c50e8db498162b784c449607fd53c
Stephen "Rixius" Middleton <stephen+git@rixi.us>
2014-05-01 06:11:04 -0500

    deck update, require authentication

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,4 +1,5 @@
 class DecksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_deck, only: [:show, :edit, :update, :destroy]
 
   # GET /decks
